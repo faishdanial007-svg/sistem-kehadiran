1f992e6091f4b32c371ce0dde88ae1cabbc43258
Faish Danial Bin Faiesal 23225183 <faishdanial007@gmail.com>
2026-02-06 15:34:45 +0800

    Pembangunan Sistem Kehadiran

--- script.py
+++ script.py
@@ -1,18 +1,18 @@
-LAMPIRAN 2
+
 import os
 
 def test():
     file_name = "index.html"
     print(f"Memulakan semakan pada: {file_name}")
 
-   
+    # Menyemak jika fail index.html wujud dalam folder
     if os.path.exists(file_name):
         print("PENGESAHAN: Fail index.html ditemui.")
     else:
         print("RALAT: Fail index.html tidak dijumpai!")
         exit(1)
 
-    
+    # Menyemak jika fungsi alert() telah ditulis dalam kod
     with open(file_name, "r") as f:
         kandungan = f.read()
         if "alert(" in kandungan:
@@ -23,4 +23,4 @@ def test():
 
 if __name__ == "__main__":
     test()
- 
+    
