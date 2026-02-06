87e10d5e17f34dd3fd16e76f412b9ae0d78583c3
Faish Danial Bin Faiesal 23225183 <faishdanial007@gmail.com>
2026-02-06 15:22:19 +0800

    Initial commit: Pembangunan Sistem Kehadiran

--- script.py
+++ script.py
@@ -1,16 +1,26 @@
+LAMPIRAN 2
 import os
 
-filename = "index.html"
+def test():
+    file_name = "index.html"
+    print(f"Memulakan semakan pada: {file_name}")
 
-if os.path.exists(filename):
-    print(f"Fail '{filename}' wujud ✅")
-else:
-    print(f"Fail '{filename}' TIDAK wujud ❌")
-
-
-with open(filename, "r", encoding="utf-8") as file:
-    content = file.read()
-    if "alert(" in content:
-        print("Fungsi alert() wujud ✅")
+   
+    if os.path.exists(file_name):
+        print("PENGESAHAN: Fail index.html ditemui.")
     else:
-        print("Fungsi alert() TIDAK wujud ❌")
+        print("RALAT: Fail index.html tidak dijumpai!")
+        exit(1)
+
+    
+    with open(file_name, "r") as f:
+        kandungan = f.read()
+        if "alert(" in kandungan:
+            print("PENGESAHAN: Fungsi alert dikesan dalam kod.")
+        else:
+            print("RALAT: Fungsi alert tidak dijumpai!")
+            exit(1)
+
+if __name__ == "__main__":
+    test()
+ 
